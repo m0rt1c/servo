3d41421334128d0e4b6e6adafc590bcd35d9a582
Andrea Jegher <andreajegher@radicallyopensecurity.com>
2025-06-19 14:48:08 +0200

    Adds fuzz sample non working function

--- fuzz/fuzz_targets/components/script/dom/writablestream.rs
+++ fuzz/fuzz_targets/components/script/dom/writablestream.rs
@@ -1,27 +1,30 @@
 #![no_main]
 #[macro_use]
-extern crate libfuzzer_sys;
 
-use std::rc::Rc;
+extern crate libfuzzer_sys;
+extern crate js;
+
+use js::rust::Runtime;
 use script_bindings::codegen::GenericBindings::QueuingStrategyBinding::QueuingStrategy;
 use script_bindings::codegen::GenericBindings::WritableStreamBinding;
 use script_bindings::codegen::InheritTypes::EventTargetTypeId::GlobalScope;
-use script_bindings::codegen::PrototypeList::ID::WritableStream;
 use script_bindings::DomTypes;
 use script_bindings::error::Fallible;
 use script_bindings::root::DomRoot;
 use script_bindings::script_runtime::CanGc;
-use script_bindings::codegen::DomTypes;
 
-fuzz_target!(|data: &[u8]| {
+fuzz_target!(|data: &[u8]| unsafe{
 
-    let cx = script_bindings::script_runtime::JSContext::from_ptr(Rc::new(runtime));
+    let engine = js::rust::JSEngine::init().unwrap();
+    let rt = Runtime::new(engine.handle());
+    let cx = script_bindings::script_runtime::JSContext::from_ptr(rt.cx());
+    
     let proto = None;
     let can_gc = CanGc::note();
     let underlying_sink = None;
-    let strategy =QueuingStrategy::empty();
+    let strategy: QueuingStrategy<WritableStream> = QueuingStrategy::empty();
 
-    let fallible : Fallible<DomRoot<DomTypes::WritableStream>> = WritableStreamBinding::WritableStreamMethods::Constructor(
+    let fallible: Fallible<DomRoot<WritableStream>> = WritableStreamBinding::WritableStreamMethods::Constructor(
         cx,
         &GlobalScope,
         proto,
